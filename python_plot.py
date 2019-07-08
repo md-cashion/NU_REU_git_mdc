1333fe18d750b69973075f396020f4999757fe85
Melissa Cashion <md_cashion@tamu.edu>
2019-07-08 13:49:29 -0500

    initial commit

---
+++ python_plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig,ax=plt.subplots(figsize=(8,5))
+x = np.linspace(0,2*np.pi,1000)
+y = np.sin(x)
+ax.plot(x,y)
+ax.set_xlim(0,2*np.pi)
+fig.tightlayout()
+
+
+
